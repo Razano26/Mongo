90c65833485b219c7775c9d6a0caaa0e7ce0195e
Thomas <broine.thomas@gmail.com>
2024-04-27 18:27:51 +0200

    Clean import + add command

--- backend/import.js
+++ backend/import.js
@@ -1,58 +1,68 @@
-// Importation des modules nécessaires
-const mongoose = require('mongoose'); // Pour interagir avec MongoDB
-const fs = require('fs'); // Pour lire les fichiers du système
+const fs = require('fs');
+const csv = require('csv-parser');
+const { MongoClient } = require('mongodb');
 
-// Chaîne de connexion à la base de données MongoDB
-const dbURI =
-  'mongodb://myuser:mypassword@localhost:27017/mydatabase?authSource=admin';
+const uri = "mongodb://myuser:mypassword@localhost:27017";
+const client = new MongoClient(uri);
 
-// Connexion à la base de données
-mongoose
-  .connect(dbURI)
-  .then(() => console.log('MongoDB connected...')) // En cas de succès de la connexion
-  .catch((err) => console.error(err)); // En cas d'erreur de connexion
+const dbName = 'import';
+const collectionName = 'etablissements';
 
-// Définition du schéma pour la géométrie
-const GeometrySchema = new mongoose.Schema({
-  type: { type: String, required: true, enum: ['Point'] }, // Le type de géométrie (ici, seulement 'Point' est accepté)
-  coordinates: { type: [Number], required: true }, // Les coordonnées de la géométrie
-});
-
-// Définition du schéma pour les données
-const DataSchema = new mongoose.Schema(
-  {
-    type: String, // Le type de données
-    properties: mongoose.Schema.Types.Mixed, // Les propriétés des données (peuvent être de n'importe quel type)
-    geometry: GeometrySchema, // La géométrie des données
-  },
-  { strict: false },
-); // L'option 'strict' est définie sur false pour permettre l'ajout de champs qui ne sont pas dans le schéma
-
-// Création du modèle de données à partir du schéma
-const DataModel = mongoose.model('data', DataSchema);
-
-// Fonction pour importer les données
-async function importData() {
-  try {
-    // Chemin du fichier à importer
-    const filePath = '../Data/OSM_Metropole_restauration_bar.json';
-    console.log('Attempting to read from:', fs.realpathSync(filePath)); // Tentative de lecture du fichier
-
-    // Lecture et analyse du fichier JSON
-    const rawData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-    // Extraction des caractéristiques des données brutes
-    const data = rawData.features;
-
-    // Insertion des données dans la base de données
-    const result = await DataModel.insertMany(data);
-    console.log(`${result.length} documents were inserted`); // Affichage du nombre de documents insérés
-  } catch (error) {
-    console.error('Failed to import data:', error); // En cas d'erreur, affichage de l'erreur
-  } finally {
-    mongoose.disconnect(); // Déconnexion de la base de données
-  }
+function parseTags(tagsString) {
+    const tagsArray = tagsString.split(', ');
+    const tagsObj = {};
+    tagsArray.forEach(pair => {
+        const [key, value] = pair.split('=>');
+        if (key && value) {
+            const cleanKey = key.trim().replace(/^"|"$/g, '');
+            const cleanValue = value.trim().replace(/^"|"$/g, '');
+            const formattedKey = cleanKey.includes(':') ? cleanKey.replace(/:/g, '.') : cleanKey;
+            tagsObj[formattedKey] = cleanValue;
+        }
+    });
+    return tagsObj;
 }
 
-// Appel de la fonction d'importation des données
-importData();
+async function main() {
+    try {
+        await client.connect();
+        console.log("Connecté à MongoDB");
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        const results = [];
+
+        fs.createReadStream('../Data/OSM_Metropole_restauration_bar.csv')
+            .pipe(csv())
+            .on('data', (data) => {
+                const tagsData = parseTags(data.tags);
+                const cleanData = {
+                    osmId: data.osm_id,
+                    street: data.addr_street,
+                    houseNumber: data.addr_housenumber,
+                    amenity: data.amenity,
+                    name: data.name,
+                    brand: data.brand,
+                    openingHours: data.opening_hours,
+                    timestamp: data.osm_timestamp,
+                    ...tagsData
+                };
+                results.push(cleanData); 
+            })
+            .on('end', async () => {
+                if (results.length > 0) {
+                    await collection.insertMany(results);
+                }
+                console.log(`Insertion de ${results.length} enregistrements réussie.`);
+                await client.close();
+                console.log("Connexion MongoDB fermée.");
+            })
+            .on('error', (error) => {
+                console.error("Erreur lors de la lecture du fichier CSV :", error);
+            });
+    } catch (error) {
+        console.error("Erreur MongoDB :", error);
+    }
+}
+
+main().catch(console.error);
