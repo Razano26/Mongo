1da966939aedb1c4e52932261f6a30be8f28a6d4
Thomas <broine.thomas@gmail.com>
2024-04-30 01:05:43 +0200

    WIP aggregate

--- backend/src/bar/bars.service.ts
+++ backend/src/bar/bars.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { Bar } from '../schemas/Bar.schema';
+import { Bar } from '../schemas/bar.schema';
 import { CreateBarDto } from './dto/createBar.dto';
 import { PatchBarDto } from './dto/patchBar.dto';
 
